873829a5c62fc96fdc05841e6e39e6f9b622f65e
RahafSmairat <rahaf.alsmairat@gmail.com>
2025-01-18 21:56:31 +0300

    home page in progress

---
+++ javaScript/navAndFooterFetch.js
@@ -0,0 +1,7 @@
+fetch('navbar.html')
+.then(response => response.text())
+.then(data => document.getElementById('nav').innerHTML = data);
+
+fetch('footer.html')
+.then(response => response.text())
+.then(data => document.getElementById('footer').innerHTML = data);
